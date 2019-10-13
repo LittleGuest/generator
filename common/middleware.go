0288586fa151bbc58368c0ed065ad30202dee5dc
littleguest <2190975784@qq.com>
2019-10-13 20:51:47 +0800

    add views

--- common/middleware.go
+++ common/middleware.go
@@ -14,20 +14,17 @@ func NewMiddleware() Middleware {
 
 // 设置跨域请求
 func (m Middleware) CorsHandler(handler http.Handler) http.Handler {
-	log.Println("CorsHandler")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		//if r.Method == http.MethodOptions {
-		//	return
-		//}
+		if r.Method == http.MethodOptions {
+			return
+		}
 		handler.ServeHTTP(w, r)
-		log.Println("设置了跨域请求")
 	})
 }
 
 // 记录请求时间
 func (m Middleware) RequestTimeHandler(handler http.Handler) http.Handler {
-	log.Println("RequestTimeHandler")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		handler.ServeHTTP(w, r)
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,8 +16,14 @@ func main() {
 	router.Use(middle.RequestTimeHandler, middle.CorsHandler)
 	router.HandleFunc("/api/db/save", generator.SaveDB)
 	router.HandleFunc("/api/db", generator.ListDB).Methods(http.MethodGet)
-	//router.PathPrefix("/views/").Handler(http.StripPrefix("/views/", http.FileServer(http.Dir("views"))))
+	router.PathPrefix("").Handler(http.StripPrefix("", http.FileServer(http.Dir("views"))))
 
 	log.Println("run at localhost:65535")
-	log.Println(http.ListenAndServe(":65535", router))
+	server := &http.Server{
+		Addr:         ":65535",
+		Handler:      router,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+	log.Println(server.ListenAndServe())
 }
